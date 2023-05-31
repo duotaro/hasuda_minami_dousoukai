be7ed8e02da568d8d3d8409a644cabcc4aff4fbe
duotaro <taro8815@gmail.com>
2023-05-31 22:48:25 +0900

    fixed

---
+++ src/utils/const.js
@@ -0,0 +1,33 @@
+let CONST = {}
+/**
+ * 同級生のデータ
+ * firebaseに事前登録するデータ
+ * 別にいらないけど
+ */
+const menber =[
+    {
+        id: 1,
+        name: '同級生1',
+        emailAddress: '',
+        password: '',
+        address: ''
+    },
+    {
+        id: 2,
+        name: '同級生2',
+        emailAddress: '',
+        password: '',
+        address: ''
+    }
+]
+CONST.menber = menber
+
+
+
+
+
+
+
+
+
+export default CONST
